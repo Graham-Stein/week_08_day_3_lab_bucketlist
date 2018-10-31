8cb2343d34f25038f84c8548910ca93608cd0233
oksana richards <oksana_vldk@hotmail.com>
2018-10-31 15:35:58 +0000

    work on pub_sub routes

--- client/src/models/bucket_list.js
+++ client/src/models/bucket_list.js
@@ -1,28 +1,37 @@
 const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
-const BucketList = function (url) {
+const BucketList = function(url) {
   this.url = url;
   this.request = new RequestHelper(this.url);
 };
 
-// BucketList.prototype.bindEvents = function () {
-//   PubSub.subscribe('BucketListView:bucketListItem-delete-clicked', (evt) => {
-//     this.deleteSighting(evt.detail);
-//   });
-//   PubSub.subscribe('SightingFormView:sighting-submitted', (evt) => {
-//     // console.log("EVT DETAIL IN MODEL:", evt.detail);
-//     this.postSighting(evt.detail);
-//   })
-  //TODO another subscribe to new form being created
+BucketList.prototype.bindEvents = function() {
+  // PubSub.subscribe('BucketListView:bucketListItem-delete-clicked', (evt) => {
+  //   this.deleteSighting(evt.detail);
+  // });
+  PubSub.subscribe('BucketListFormView:bucket-list-item-submitted', (evt) => {
+    console.log('EVT DETAIL IN MODEL:', evt.detail);
+    this.postBucketItem(evt.detail);
+  });
+};
+BucketList.prototype.postBucketItem = function(item) {
+  console.log(item);
+  this.request.post(item)
+      .then((items) => {
+        PubSub.publish('BucketList:data-loaded', items);
+      })
+      .catch(console.error);
+};
+  // To do another subscribe to new form being created
   //make post request to server to add new data
 
-BucketList.prototype.getData = function () {
+BucketList.prototype.getData = function() {
   this.request.get()
-    .then((bucketListItems) => {
-      PubSub.publish('BucketList:data-loaded', bucketListItems);
-    })
-    .catch(console.error);
+      .then((bucketListItems) => {
+        PubSub.publish('BucketList:data-loaded', bucketListItems);
+      })
+      .catch(console.error);
 };
 
 module.exports = BucketList;
